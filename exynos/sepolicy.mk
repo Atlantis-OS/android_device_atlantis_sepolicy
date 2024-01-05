22ebeda87410e41513db4fd7d3c6d5dfc83c4916
janak <niraulajanak2019@gmail.com>
2024-01-05 21:19:18 +0545

    sepolicy: Adapt for AtlantisOS
    Signed-off-by: janak <niraulajanak2019@gmail.com>

--- exynos/sepolicy.mk
+++ exynos/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all exynos products
-# that inherit from Lineage
+# that inherit from Atlantis
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,14 +10,14 @@ endif
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/private
+    device/atlantis/sepolicy/exynos/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/system
+    device/atlantis/sepolicy/exynos/dynamic \
+    device/atlantis/sepolicy/exynos/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/vendor
+    device/atlantis/sepolicy/exynos/dynamic \
+    device/atlantis/sepolicy/exynos/vendor
 endif
